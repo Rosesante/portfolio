2f1a1fd9635ce5b0a85810b952c586266148ecf8
Roseey <rosesantem@gmail.com>
2026-04-14 18:50:16 +0300

    Update portfolio content

--- my_self/sante/views.py
+++ my_self/sante/views.py
@@ -7,7 +7,15 @@ from .forms import ContactForm
 #Home page
 def home(request):
     info = Portfolio.objects.first() #get my data
-    return render(request, "sante/home.html", {"info": info})
+    skills_list = []
+    if info and info.skills:
+        skills_list = [s.strip() for s in info.skills.split(",") if s.strip()]
+    projects = Project.objects.all()[:3]
+    return render(request, "sante/home.html", {
+        "info": info,
+        "skills_list": skills_list,
+        "projects": projects,
+    })
 
 
 #Contact page
@@ -16,11 +24,10 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, "sante/contact_success.html")
     else:
         form = ContactForm()
 
-    return render(request, "sante/contact_success.html",)
+    return render(request, "sante/contact.html", {"form": form})
 
 
 #About page
@@ -28,14 +35,52 @@ def about(request):
     info = Portfolio.objects.first()
     education = Education.objects.all()
     experience = Experience.objects.all()
+    skills_list = []
+    if info and info.skills:
+        skills_list = [s.strip() for s in info.skills.split(",") if s.strip()]
+    skill_icon_map = {
+        "html": "fa-html5",
+        "css": "fa-css3-alt",
+        "javascript": "fa-js",
+        "python": "fa-python",
+        "django": "fa-django",
+        "react": "fa-react",
+        "bootstrap": "fa-bootstrap",
+        "git": "fa-git-alt",
+        "github": "fa-github",
+    }
+    default_progress = 80
+    progress_map = {
+        "html": 80,
+        "css": 80,
+        "javascript": 70,
+        "js": 70,
+        "django": 78,
+        "react": 78,
+    }
+    skills_with_meta = []
+    for skill in skills_list:
+        key = skill.lower()
+        icon = skill_icon_map.get(key, "fa-code")
+        progress = progress_map.get(key, default_progress)
+        skills_with_meta.append({
+            "name": skill,
+            "icon": icon,
+            "progress": progress,
+        })
     return render(request, "sante/about.html", {
         "info": info,
         "education": education,
-        "experience": experience
+        "experience": experience,
+        "skills_with_meta": skills_with_meta,
     })
 
 
 #Projects page
 def projects(request):
     projects = Project.objects.all()
-    return render(request, "sante/projects.html", {'projects': projects}) 
+    info = Portfolio.objects.first()
+    return render(request, "sante/projects.html", {
+        "projects": projects,
+        "info": info,
+    }) 
